840552e728766cbf333803af6a686d9296ef5576
sheppardjm <jsheppard@neucadia.com>
2024-10-04 23:32:12 -0400

    Add equine feed list json and map list

--- src/pages/equine/index.js
+++ src/pages/equine/index.js
@@ -205,6 +205,69 @@ const logos = [
   },
 ];
 
+const feed = [
+  {
+    id: 1,
+    title: "Harvey's Horse 10%",
+    protein: "10%",
+    weight: "50 lbs",
+  },
+  {
+    id: 2,
+    title: "Buckeye - Supreme 14",
+    protein: "14%",
+    weight: "40 lbs",
+  },
+  {
+    id: 3,
+    title: "Buckeye - Safe 'N Easy Senior Pelleted",
+    protein: "14%",
+    weight: "40 lbs",
+  },
+  {
+    id: 4,
+    title: "Buckeye - Safe 'N Easy Complete",
+    protein: "12%",
+    weight: "50 lbs",
+  },
+  {
+    id: 5,
+    title: "Buckeye - Ultimate Finish 25",
+    protein: "12%",
+    weight: "40 lbs",
+  },
+  {
+    id: 6,
+    title: "Buckeye - Gro 'N Win",
+    protein: "32%",
+    weight: "50 lbs",
+  },
+  {
+    id: 7,
+    title: "Buckeye - EQ8 Gut Health",
+    protein: "12%",
+    weight: "50 lbs",
+  },
+  {
+    id: 8,
+    title: "Buckeye - Apple Snap Treats",
+    protein: "12%",
+    weight: "4 lbs",
+  },
+  {
+    id: 9,
+    title: "Buckeye - Carrot Crunchers Treats",
+    protein: "12%",
+    weight: "4 lbs",
+  },
+  {
+    id: 10,
+    title: "Buckeye - Peppermint Bits Treats",
+    protein: "12%",
+    weight: "4 lbs",
+  },
+];
+
 const EquinePage = () => {
   const [activeLogo, setActiveLogo] = React.useState(logos[0]);
   const [navOpen, setNavOpen] = React.useState(false);
@@ -405,60 +468,62 @@ const EquinePage = () => {
                       well as blending our own proprietary and custom mixes.
                     </p>
                   </div>
-                  <div className="flex flex-wrap -m-3">
-                    <div className="w-full p-3">
-                      <div className="p-10 bg-gray-900 rounded-3xl">
-                        <div className="flex flex-wrap items-center md:items-start -m-2">
-                          <div className="w-full md:w-1/2 p-2">
-                            <h2 className="font-heading text-2xl text-white font-black tracking-tight">
-                              Harvey's Horse 10%
-                            </h2>
-                          </div>
-                          <div className="w-full md:w-1/2 p-2">
-                            <div className="flex flex-wrap items-center justify-end -m-5">
-                              <div className="w-full md:w-auto p-5">
-                                <div className="flex flex-wrap -m-3">
-                                  <div className="flex flex-wrap items-center w-auto p-3">
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      fill="#374151"
-                                      width={24}
-                                      height={24}
-                                      viewBox="0 0 24 24"
-                                      className="bi bi-hammer mr-2"
-                                    >
-                                      <path d="M20.57 14.86 22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22l1.43-1.43L16.29 22l2.14-2.14 1.43 1.43 1.43-1.43-1.43-1.43L22 16.29l-1.43-1.43z" />
-                                    </svg>
-                                    <p className="text-gray-500 font-bold">
-                                      10%
-                                    </p>
-                                  </div>
-                                  <div className="flex flex-wrap items-center w-auto p-3">
-                                    <svg
-                                      xmlns="http://www.w3.org/2000/svg"
-                                      fill="#374151"
-                                      width={24}
-                                      height={24}
-                                      viewBox="0 0 24 24"
-                                      className="mr-2"
-                                    >
-                                      <path d="M14 11V8c4.56-.58 8-3.1 8-6H2c0 2.9 3.44 5.42 8 6v3c-3.68.73-8 3.61-8 11h6v-2H4.13c.93-6.83 6.65-7.2 7.87-7.2s6.94.37 7.87 7.2H16v2h6c0-7.39-4.32-10.27-8-11zm4.87-7C17.5 5.19 15 6.12 12 6.12S6.5 5.19 5.13 4h13.74zM12 22c-1.1 0-2-.9-2-2 0-.55.22-1.05.59-1.41C11.39 17.79 16 16 16 16s-1.79 4.61-2.59 5.41c-.36.37-.86.59-1.41.59z" />
-                                    </svg>
-                                    <p className="text-gray-500 font-bold">
-                                      50 lbs
-                                    </p>
+                  {feed.map((item) => (
+                    <div className="flex flex-wrap -m-3">
+                      <div className="w-full p-3">
+                        <div className="p-10 bg-gray-900 rounded-3xl">
+                          <div className="flex flex-wrap items-center md:items-start -m-2">
+                            <div className="w-full md:w-1/2 p-2">
+                              <h2 className="font-heading text-2xl text-white font-black tracking-tight">
+                                {item.title}
+                              </h2>
+                            </div>
+                            <div className="w-full md:w-1/2 p-2">
+                              <div className="flex flex-wrap items-center justify-end -m-5">
+                                <div className="w-full md:w-auto p-5">
+                                  <div className="flex flex-wrap -m-3">
+                                    <div className="flex flex-wrap items-center w-auto p-3">
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="#374151"
+                                        width={24}
+                                        height={24}
+                                        viewBox="0 0 24 24"
+                                        className="bi bi-hammer mr-2"
+                                      >
+                                        <path d="M20.57 14.86 22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22l1.43-1.43L16.29 22l2.14-2.14 1.43 1.43 1.43-1.43-1.43-1.43L22 16.29l-1.43-1.43z" />
+                                      </svg>
+                                      <p className="text-gray-500 font-bold">
+                                        {item.protein}
+                                      </p>
+                                    </div>
+                                    <div className="flex flex-wrap items-center w-auto p-3">
+                                      <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="#374151"
+                                        width={24}
+                                        height={24}
+                                        viewBox="0 0 24 24"
+                                        className="mr-2"
+                                      >
+                                        <path d="M14 11V8c4.56-.58 8-3.1 8-6H2c0 2.9 3.44 5.42 8 6v3c-3.68.73-8 3.61-8 11h6v-2H4.13c.93-6.83 6.65-7.2 7.87-7.2s6.94.37 7.87 7.2H16v2h6c0-7.39-4.32-10.27-8-11zm4.87-7C17.5 5.19 15 6.12 12 6.12S6.5 5.19 5.13 4h13.74zM12 22c-1.1 0-2-.9-2-2 0-.55.22-1.05.59-1.41C11.39 17.79 16 16 16 16s-1.79 4.61-2.59 5.41c-.36.37-.86.59-1.41.59z" />
+                                      </svg>
+                                      <p className="text-gray-500 font-bold">
+                                        {item.weight}
+                                      </p>
+                                    </div>
                                   </div>
                                 </div>
-                              </div>
-                              <div className="w-full md:w-auto p-5">
-                                <div className="flex flex-wrap md:justify-center -m-2">
-                                  <div className="w-full md:w-auto p-2">
-                                    <a
-                                      className="block w-full px-4 py-2.5 text-sm text-center font-bold focus:ring-4 rounded-full bg-green-500 focus:ring-green-50 hover:bg-green-600"
-                                      href="#"
-                                    >
-                                      Buy Now
-                                    </a>
+                                <div className="w-full md:w-auto p-5">
+                                  <div className="flex flex-wrap md:justify-center -m-2">
+                                    <div className="w-full md:w-auto p-2">
+                                      <a
+                                        className="block w-full px-4 py-2.5 text-sm text-center font-bold focus:ring-4 rounded-full bg-green-500 focus:ring-green-50 hover:bg-green-600"
+                                        href="#"
+                                      >
+                                        Buy Now
+                                      </a>
+                                    </div>
                                   </div>
                                 </div>
                               </div>
@@ -467,7 +532,7 @@ const EquinePage = () => {
                         </div>
                       </div>
                     </div>
-                  </div>
+                  ))}
                 </div>
               </div>
             </div>
