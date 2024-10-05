ec65448074d85ca7bb2bff0f7d38182fab38b33a
sheppardjm <jsheppard@neucadia.com>
2024-10-04 23:14:15 -0400

    Add empty equine list Fix tailwind classes not building

---
+++ src/pages/equine/EquinelistSectionCareersDark1.js
@@ -0,0 +1,91 @@
+import React from "react";
+
+export default function EquinelistSectionCareersDark1() {
+  return (
+    <React.Fragment>
+      <>
+        <section className="py-10 bg-gray-900 overflow-hidden">
+          <div className="container mx-auto px-4">
+            <div className="py-16 px-8 bg-gray-800 rounded-3xl">
+              <div className="max-w-7xl mx-auto">
+                <div className="mb-10 md:max-w-4xl mx-auto text-center">
+                  <span className="inline-block mb-4 text-sm font-bold uppercase tracking-widest text-green-500">
+                    Harvey's Feed
+                  </span>
+                  <h2 className="font-heading mb-6 text-4xl md:text-5xl lg:text-6xl text-white font-black tracking-tight">
+                    Equine Mixes
+                  </h2>
+                  <p className="md:max-w-md mx-auto text-gray-500 font-bold">
+                    Harvey's carries stock mixes from Buckeye Nutrition as well
+                    as blending our own proprietary and custom mixes.
+                  </p>
+                </div>
+                <div className="flex flex-wrap -m-3">
+                  <div className="w-full p-3">
+                    <div className="p-10 bg-gray-900 rounded-3xl">
+                      <div className="flex flex-wrap items-center -m-2">
+                        <div className="w-full md:w-1/2 p-2">
+                          <h2 className="font-heading text-2xl text-white font-black tracking-tight">
+                            Harvey's Horse 10%
+                          </h2>
+                        </div>
+                        <div className="w-full md:w-1/2 p-2">
+                          <div className="flex flex-wrap items-center justify-end -m-5">
+                            <div className="w-full md:w-auto p-5">
+                              <div className="flex flex-wrap -m-3">
+                                <div className="flex flex-wrap items-center w-auto p-3">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="#374151"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 24 24"
+                                    className="bi bi-hammer mr-2"
+                                  >
+                                    <path d="M20.57 14.86 22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22l1.43-1.43L16.29 22l2.14-2.14 1.43 1.43 1.43-1.43-1.43-1.43L22 16.29l-1.43-1.43z" />
+                                  </svg>
+                                  <p className="text-gray-500 font-bold">10%</p>
+                                </div>
+                                <div className="flex flex-wrap items-center w-auto p-3">
+                                  <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="#374151"
+                                    width={24}
+                                    height={24}
+                                    viewBox="0 0 24 24"
+                                    className="mr-2"
+                                  >
+                                    <path d="M14 11V8c4.56-.58 8-3.1 8-6H2c0 2.9 3.44 5.42 8 6v3c-3.68.73-8 3.61-8 11h6v-2H4.13c.93-6.83 6.65-7.2 7.87-7.2s6.94.37 7.87 7.2H16v2h6c0-7.39-4.32-10.27-8-11zm4.87-7C17.5 5.19 15 6.12 12 6.12S6.5 5.19 5.13 4h13.74zM12 22c-1.1 0-2-.9-2-2 0-.55.22-1.05.59-1.41C11.39 17.79 16 16 16 16s-1.79 4.61-2.59 5.41c-.36.37-.86.59-1.41.59z" />
+                                  </svg>
+                                  <p className="text-gray-500 font-bold">
+                                    50 lbs
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="w-full md:w-auto p-5">
+                              <div className="flex flex-wrap md:justify-center -m-2">
+                                <div className="w-full md:w-auto p-2">
+                                  <a
+                                    className="block w-full px-4 py-2.5 text-sm text-center font-bold focus:ring-4 rounded-full bg-green-500 focus:ring-green-50 hover:bg-green-600"
+                                    href="#"
+                                  >
+                                    Buy Now
+                                  </a>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    </React.Fragment>
+  );
+}
--- src/pages/equine/index.js
+++ src/pages/equine/index.js
@@ -196,39 +196,18 @@ const menu = [
   },
 ];
 
-export default function IndexSectionHeadersDarkReversePattern1({
-  activeLogo,
-  changeLogo,
-}) {
-  React.useEffect(() => {
-    var promise = ref.current.pause();
+const logos = [
+  {
+    name: "tiller",
+  },
+  {
+    name: "script",
+  },
+];
 
-    if (promise !== undefined) {
-      promise
-        .then((_) => {
-          // Autoplay started!
-        })
-        .catch((error) => {
-          // Autoplay was prevented.
-          // Show a "Play" button so that user can start playback.
-          console.log(error);
-        });
-    }
-  }, []);
-  const [isPlaying, setIsPlaying] = React.useState(false);
+const EquinePage = () => {
+  const [activeLogo, setActiveLogo] = React.useState(logos[0]);
   const [navOpen, setNavOpen] = React.useState(false);
-  const ref = React.useRef(null);
-
-  const handleClick = () => {
-    const nextIsPlaying = !isPlaying;
-    setIsPlaying(nextIsPlaying);
-
-    if (nextIsPlaying) {
-      ref.current.play();
-    } else {
-      ref.current.pause();
-    }
-  };
 
   const linkTransformer = (menuItem) => {
     return (
@@ -259,7 +238,7 @@ export default function IndexSectionHeadersDarkReversePattern1({
                     <div className="w-auto">
                       {" "}
                       {/* eslint-disable-next-line */}
-                      <a href="#" onClick={changeLogo}>
+                      <a href="#">
                         {" "}
                         <img
                           src={`/zanrly-assets/logos/harveys-${activeLogo.name}-white.svg`}
@@ -358,11 +337,7 @@ export default function IndexSectionHeadersDarkReversePattern1({
                         <div className="w-auto p-2">
                           {" "}
                           {/* eslint-disable-next-line */}
-                          <a
-                            className="inline-block"
-                            href="#"
-                            onClick={changeLogo}
-                          >
+                          <a className="inline-block" href="#">
                             {" "}
                             <img
                               src={`/zanrly-assets/logos/harveys-${activeLogo.name}-white.svg`}
@@ -413,67 +388,94 @@ export default function IndexSectionHeadersDarkReversePattern1({
                 </nav>{" "}
               </div>{" "}
             </div>{" "}
-            <div className="relative bg-gray-900 overflow-hidden rounded-t-3xl">
-              {" "}
-              <div className="absolute top-1/2 left-1/2 min-w-max transform -translate-x-1/2 -translate-y-1/2">
-                {" "}
-                <div className="absolute bg-gradient-radial-dark w-full h-full" />{" "}
-                <img
-                  src="zanrly-assets/images/pattern-dark.png"
-                  alt="pattern"
-                />{" "}
-              </div>{" "}
-              <div className="relative px-8 pt-20">
-                {" "}
-                <div className="md:max-w-2xl text-center mx-auto">
-                  {" "}
-                  <span className="inline-block mb-3 text-sm font-bold uppercase tracking-widest text-green-500">
-                    WELCOME TO HARVEY'S
-                  </span>{" "}
-                  <h1 className="font-heading mb-6 text-5xl lg:text-6xl text-white font-black tracking-tight">
-                    A family of agriculture-focused companies
-                  </h1>{" "}
-                  <p className="mb-8 text-xl text-gray-500 font-bold">
-                    Harvey's has grown from a single feed mill into a
-                    multi-disciplinary group of direct-to-consumer and B2B
-                    ventures
-                  </p>{" "}
-                  <div className="flex flex-wrap justify-center mb-20 -m-2" />{" "}
-                </div>{" "}
-              </div>{" "}
-            </div>{" "}
-            <div className="relative">
-              {" "}
-              <div className="relative z-20 px-8 max-w-max mx-auto">
-                {" "}
-                <video
-                  muted
-                  loop
-                  poster="https://static.shuffle.dev/uploads/files/cc/cce6580999c8067e23bb4a662dea535a74b463e5/Screen-Shot-2024-09-12-at-12-19-20-PM.jpg"
-                  id="bgvid"
-                  ref={ref}
-                  onPlay={() => setIsPlaying(true)}
-                  onPause={() => setIsPlaying(false)}
-                >
-                  <source src="bg.webm" type="video/webm" />
-                  <source src="bg.mp4" type="video/mp4" />
-                </video>
-                {/* eslint-disable-next-line */}
-                <a
-                  className={`absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 inline-block px-8 py-3.5 text-lg text-center text-gray-900 font-bold bg-gray-50 hover:bg-gray-200 focus:ring-4 focus:ring-gray-200 rounded-full ${
-                    isPlaying ? `hidden` : ``
-                  }`}
-                  onClick={handleClick}
-                  tabIndex={-1}
-                >
-                  Watch Video
-                </a>{" "}
-              </div>{" "}
-              <div className="absolute left-0 top-0 w-full h-1/2 bg-gray-900 rounded-b-3xl" />{" "}
-            </div>
           </div>
+          <section className="py-10 bg-gray-900 overflow-hidden">
+            <div className="container mx-auto px-4">
+              <div className="py-16 px-8 bg-gray-800 rounded-3xl">
+                <div className="max-w-7xl mx-auto">
+                  <div className="mb-10 md:max-w-4xl mx-auto text-center">
+                    <span className="inline-block mb-4 text-sm font-bold uppercase tracking-widest text-green-500">
+                      Harvey's Feed
+                    </span>
+                    <h2 className="font-heading mb-6 text-4xl md:text-5xl lg:text-6xl text-white font-black tracking-tight">
+                      Equine Mixes
+                    </h2>
+                    <p className="md:max-w-md mx-auto text-gray-500 font-bold">
+                      Harvey's carries stock mixes from Buckeye Nutrition as
+                      well as blending our own proprietary and custom mixes.
+                    </p>
+                  </div>
+                  <div className="flex flex-wrap -m-3">
+                    <div className="w-full p-3">
+                      <div className="p-10 bg-gray-900 rounded-3xl">
+                        <div className="flex flex-wrap items-center md:items-start -m-2">
+                          <div className="w-full md:w-1/2 p-2">
+                            <h2 className="font-heading text-2xl text-white font-black tracking-tight">
+                              Harvey's Horse 10%
+                            </h2>
+                          </div>
+                          <div className="w-full md:w-1/2 p-2">
+                            <div className="flex flex-wrap items-center justify-end -m-5">
+                              <div className="w-full md:w-auto p-5">
+                                <div className="flex flex-wrap -m-3">
+                                  <div className="flex flex-wrap items-center w-auto p-3">
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      fill="#374151"
+                                      width={24}
+                                      height={24}
+                                      viewBox="0 0 24 24"
+                                      className="bi bi-hammer mr-2"
+                                    >
+                                      <path d="M20.57 14.86 22 13.43 20.57 12 17 15.57 8.43 7 12 3.43 10.57 2 9.14 3.43 7.71 2 5.57 4.14 4.14 2.71 2.71 4.14l1.43 1.43L2 7.71l1.43 1.43L2 10.57 3.43 12 7 8.43 15.57 17 12 20.57 13.43 22l1.43-1.43L16.29 22l2.14-2.14 1.43 1.43 1.43-1.43-1.43-1.43L22 16.29l-1.43-1.43z" />
+                                    </svg>
+                                    <p className="text-gray-500 font-bold">
+                                      10%
+                                    </p>
+                                  </div>
+                                  <div className="flex flex-wrap items-center w-auto p-3">
+                                    <svg
+                                      xmlns="http://www.w3.org/2000/svg"
+                                      fill="#374151"
+                                      width={24}
+                                      height={24}
+                                      viewBox="0 0 24 24"
+                                      className="mr-2"
+                                    >
+                                      <path d="M14 11V8c4.56-.58 8-3.1 8-6H2c0 2.9 3.44 5.42 8 6v3c-3.68.73-8 3.61-8 11h6v-2H4.13c.93-6.83 6.65-7.2 7.87-7.2s6.94.37 7.87 7.2H16v2h6c0-7.39-4.32-10.27-8-11zm4.87-7C17.5 5.19 15 6.12 12 6.12S6.5 5.19 5.13 4h13.74zM12 22c-1.1 0-2-.9-2-2 0-.55.22-1.05.59-1.41C11.39 17.79 16 16 16 16s-1.79 4.61-2.59 5.41c-.36.37-.86.59-1.41.59z" />
+                                    </svg>
+                                    <p className="text-gray-500 font-bold">
+                                      50 lbs
+                                    </p>
+                                  </div>
+                                </div>
+                              </div>
+                              <div className="w-full md:w-auto p-5">
+                                <div className="flex flex-wrap md:justify-center -m-2">
+                                  <div className="w-full md:w-auto p-2">
+                                    <a
+                                      className="block w-full px-4 py-2.5 text-sm text-center font-bold focus:ring-4 rounded-full bg-green-500 focus:ring-green-50 hover:bg-green-600"
+                                      href="#"
+                                    >
+                                      Buy Now
+                                    </a>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </section>
       </>
     </React.Fragment>
   );
-}
+};
+
+export default EquinePage;
